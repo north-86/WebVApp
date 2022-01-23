474241672e857678c05c261b8d16817935751d8e
northernman86 <aliaksandrprystauka@gmail.com>
2022-01-23 20:30:10 +0300

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace WebVApp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            Uri uri = new Uri(textBox.Text);
+            webView.Navigate(uri);
+        }
+
+        private void Bookmarks_Click(object sender, RoutedEventArgs e)
+        {
+            //at work
+        }
+    }
+}
